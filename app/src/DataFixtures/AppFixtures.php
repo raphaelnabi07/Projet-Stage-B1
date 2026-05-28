4ef1fac48905b5321c73d47059878257bb0f0c01
Raphaël Nabi <raphae.nabi@efrei.net>
2026-05-28 15:15:54 +0200

    Ajout de la fixture

---
+++ app/src/DataFixtures/AppFixtures.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use App\Entity\User;
+
+class UserFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $admin = new User();
+        $admin->setEmail('Admin@gmail.com');
+        $admin->setRoles(['ROLE_ADMIN']);
+        $admin->setPassword('Test123'); 
+        $manager->persist($admin);
+
+        $user = new User();
+        $user->setEmail('User@gmail.com');
+        $user->setRoles(['ROLE_USER']);
+        $user->setPassword('Test123');
+        $manager->persist($user);
+
+        $manager->flush();
+    }
+}
