8b2e707020bb0fafc38a0b3adb73af36cc0d3bb4
Raphaël Nabi <raphae.nabi@efrei.net>
2026-05-28 16:15:57 +0200

    Correction bug fixture

--- app/src/DataFixtures/AppFixtures.php
+++ app/src/DataFixtures/AppFixtures.php
@@ -6,20 +6,24 @@ use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use App\Entity\User;
 
-class UserFixtures extends Fixture
+class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
         $admin = new User();
         $admin->setEmail('Admin@gmail.com');
         $admin->setRoles(['ROLE_ADMIN']);
-        $admin->setPassword('Test123'); 
+        $admin->setPassword('Test123');
+        $admin->setnom('NomAdmin');
+        $admin->setprenom('PrenomAdmin');
         $manager->persist($admin);
 
         $user = new User();
         $user->setEmail('User@gmail.com');
         $user->setRoles(['ROLE_USER']);
         $user->setPassword('Test123');
+        $user->setNom('NomUser');
+        $user->setPrenom('PrenomUser');
         $manager->persist($user);
 
         $manager->flush();
